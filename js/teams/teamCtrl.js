9727c2512ae99f7e0f693e77578e3c2b21a5133a
Braydon Platt <braydonplatt@gmail.com>
2015-02-17 20:07:32 -0700

    Halfway Through the fourth part

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,28 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData,){
 
+$scope.teamData = teamData;
+$scope.newGame = {};
+$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function(){
+		$scope.showNewGameForm != $scope.showNewGameForm;
+		console.log($scope.showNewGameForm);
+	}
+
+	if(routeParams.team = 'utahjazz'){
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logoPath = 'images/jazz-logo.png';
+	}
+
+		else if(routeParams.team = 'losangeleslakers') {
+			$scope.homeTeam = 'Los Angeles Lakers';
+			$scope.logoPath = 'images/lakers-logo.png';
+		}
+
+		else if(routeParams.team = 'miamiheat') {
+			$scope.homeTeam = 'Miami Heat';
+			$scope.logoPath = 'images/heat-logo.png';
+		}
+	$scope.submitGame = 
 });
