ba22a100d1f3390fc9b738668b0fccdd37159628
Braydon Platt <braydonplatt@gmail.com>
2015-02-17 19:05:24 -0700

    Finished Step 2

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,48 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj){
+		debugger;
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore >= parseInt(gameObj.opponentScore))){
+			gameObj.wins = true;
+		} else {
+			gameObj.losses = false;
+		};
+
+		$http({
+		method: 'POST',
+		url: url,
+		data: gameObj
+		});
+	};
+
+
+
+
+	this.getTeamData = function(team){
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: "https://api.parse.com/1/classes/" + team
+		})
+		.then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0; 
+				for(var i = 0; i < results.length; i++){
+					if(results[i] === true){
+						wins++;
+					} else {
+						results[i] === false;
+						losses++;
+					}
+				}
+					results.wins = wins;
+					results.losses = losses;
+						deferred.resolve(results);
+		});
+						return deferred.promise;
+	};
+
 });
